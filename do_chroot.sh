a7b345db2e55647f229af90a24131a568406bc5a
Del <delexagon@gmail.com>
2023-08-25 17:06:19 -0400

    Removed chroots with differing project names, enabled production mode, allowed init and remove to be part of an action chain.

--- do_chroot.sh
+++ do_chroot.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
+is_production=
 
 if [ ! "$EUID" = "0" ] ; then
   echo "Not root"
   exit 1
 fi
+
+run_script_in_chroot() {
+  if [ "$is_production" ] ; then
+    "$1"
+  else
+    systemd-nspawn -D "$THIS_CHROOT" -a "$1"
+  fi
+}
+
+do_special() {
+  if [ "$1" = init ] ; then
+    if ! python3 make_chroot.py "chroots" "$CHROOT_NAME" "$DISTRIBUTION" ; then
+      echo "Making chroot failed"
+      exit 1
+    fi
+  elif [ "$1" = remove ] ; then
+    rm -rf "$THIS_CHROOT"
+    exit 0
+  else
+    if ! [ -d "$THIS_CHROOT" ] ; then
+      echo "Error: $THIS_CHROOT does not exist"
+      exit 1
+    fi
+  fi
+}
 
 do_automatic() {
   if [ -f "$OUTSIDE/$1" ] ; then
@@ -12,7 +38,7 @@ do_automatic() {
   if [ -f "$THIS_PROJECT/before_copy/$1" ] ; then
     cp "$THIS_PROJECT/before_copy/$1" "$THIS_CHROOT/root/$1"
     chmod 500 "$THIS_CHROOT/root/$1"
-    systemd-nspawn -D "$THIS_CHROOT" -a "/root/$1"
+    run_script_in_chroot "/root/$1"
   fi
   if [ -f "$COPY/$1" ] ; then
     python3 copy_files.py "$DATA" "$THIS_CHROOT" "$COPY/$1" "$THIS_CHROOT/etc/passwd" "$THIS_CHROOT/etc/group"
@@ -20,7 +46,7 @@ do_automatic() {
   if [ -f "$THIS_PROJECT/start/$1" ] ; then
     cp "$THIS_PROJECT/start/$1" "$THIS_CHROOT/root/$1"
     chmod 500 "$THIS_CHROOT/root/$1"
-    systemd-nspawn -D "$THIS_CHROOT" -a "/root/$1"
+    run_script_in_chroot "/root/$1"
   fi
 }
 
@@ -28,83 +54,22 @@ if [ ! -e 'chroots' ] ; then
   mkdir chroots
 fi
 
-# Expectations: do_chroot.sh name_of_chroot init <project_name> <-b build>
+# Expectations: do_chroot.sh name_of_chroot init <-b build>
 # Expectations: do_chroot.sh name_of_chroot load
 # Expectations: do_chroot.sh name_of_chroot remove
-action="$2"
+
 CHROOT_NAME="$1"
+
 if ! [[ "$CHROOT_NAME" =~ ^[a-zA-Z0-9_-]*$ ]] ; then
   echo "Invalid chroot name"
   exit 1
 fi
-if ! [[ "$action" =~ ^[a-zA-Z0-9_-]*$ ]] ; then
-  echo "Invalid action name"
-  exit 1
-fi
 
-shift 2
+shift 1
 export THIS_CHROOT=$(realpath chroots/"$CHROOT_NAME")
-export CONFIG_FILE=$(realpath config.txt)
 
-# ARGUMENTS
-POSITIONAL_ARGS=()
-if [ "$action" = init ] ; then 
-  while [[ $# -gt 0 ]]; do
-    case $1 in
-      -b|--build)
-        BUILD="$2"
-        shift # past argument
-        shift # past value
-        ;;
-      -*|--*)
-        echo "Unknown option <$1>"
-        exit 1
-        ;;
-      *)
-        POSITIONAL_ARGS+=("$1") # save positional arg
-        shift # past argument
-        ;;
-    esac
-  done ; set -- "${POSITIONAL_ARGS[@]}"
-  
-  export BUILD=${BUILD:-jammy}
-  
-  export PROJECT_NAME=${1:-"$CHROOT_NAME"}
-  
-elif [ "$action" = remove ] ; then
-  rm -rf "$THIS_CHROOT"
-  sed -i "/^$CHROOT_NAME /d" "$CONFIG_FILE"
-  exit 0
-else
-  if ! [ -d "$THIS_CHROOT" ] ; then
-    echo "$THIS_CHROOT does not exist"
-    exit 1
-  fi
-  # Turn on impermanence by default
-  permanent="-x"
-  while [[ $# -gt 0 ]]; do
-    case $1 in
-      -p|--permanent)
-        permanent=""
-        shift # past argument
-        shift # past value
-        ;;
-      -*|--*)
-        echo "Unknown option <$1>"
-        exit 1
-        ;;
-      *)
-        POSITIONAL_ARGS+=("$1") # save positional arg
-        shift # past argument
-        ;;
-    esac
-  done ; set -- "${POSITIONAL_ARGS[@]}"
-  
-  export PROJECT_NAME=$(awk -v name="$CHROOT_NAME" '$1 ~ name { print $2 ; exit }' "$CONFIG_FILE")
-fi
-
-# get line number (to remove config line): 
-
+# Unfortunately, we assume our chroot name is the same as our project name.
+export PROJECT_NAME="$CHROOT_NAME"
 export THIS_PROJECT=$(realpath projects/"$PROJECT_NAME")
 if [ ! -d "$THIS_PROJECT" ] ; then
   echo "Project $THIS_PROJECT does not exist"
@@ -115,23 +80,40 @@ export OUTSIDE="$THIS_PROJECT"/outside
 export DATA="$THIS_PROJECT"/data
 export COPY="$THIS_PROJECT"/copy
 export EXTRA="$THIS_PROJECT"/extra
-  
-if [ "$action" = init ] ; then
-  if ! python3 make_chroot.py "chroots" "$CHROOT_NAME" "$BUILD" ; then
-    echo "Making chroot failed"
-    exit 1
-  fi
-  
-  echo "$CHROOT_NAME $PROJECT_NAME" >> "$CONFIG_FILE"
+export CONFIG="$THIS_PROJECT"/config.sh
 
-  do_automatic "$action"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--permanent)
+      permanent=""
+      shift # past argument
+      shift # past value
+      ;;
+    # VERY DANGEROUS! Applies changes to THIS COMPUTER, not a chroot!!!
+    --production)
+      export is_production=true
+      export THIS_CHROOT="/"
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option <$1>"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done ; set -- "${POSITIONAL_ARGS[@]}"
 
-else
-  
+if [ -f "$CONFIG" ] ; then
+  . "$CONFIG"
+fi
+
+for action in "$@" ; do
+  do_special "$action"
   do_automatic "$action"
-  for other_action in "$@" ; do
-    do_automatic "$other_action"
-  done
-  
+done
+if [ ! "$is_production" ] && [ -d "$THIS_CHROOT" ] ; then
   systemd-nspawn $permanent -b -D "$THIS_CHROOT"
 fi
